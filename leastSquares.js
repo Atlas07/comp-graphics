e4175e04f204832d156c8633c8c0a51ff3af37f2
Vitalii Shapoval <vitalii.shapoval@starnavi.io>
2019-09-14 16:00:43 +0300

    First lab: tag

--- leastSquares.js
+++ leastSquares.js
@@ -9,31 +9,79 @@ if (X.length !== Y.length) {
   throw new Error('Length of X and Y have to be equal');
 }
 
+// ax + b
 const sumX = X.reduce((acc, current) => acc + current);
 let sumY = Y.reduce((acc, current) => acc + current);
-let sumXSquared = X.reduce((acc, current) => acc + current ** 2, 0);
+let sumX2 = X.reduce((acc, current) => acc + current ** 2, 0);
 let sumXY = X.reduce((acc, current, i) => acc + current * Y[i], 0);
 
-console.log('\nSums:');
-console.log('sumX', sumX);
-console.log('sumY', sumY);
-console.log('sumXSquared', sumXSquared);
-console.log('sumXY', sumXY);
-console.log('\n');
+// a ** 2 + bx + c
+const sumX4 = X.reduce((acc, current) => acc + current ** 4, 0);
+const sumX3 = X.reduce((acc, current) => acc + current ** 4, 0);
+const sumX2Y = X.reduce((acc, current, i) => acc + current ** 2 * Y[i] , 0);
 
-const [aCoefs, bCoefs] = lusolve(
-  [
-    [sumXSquared, sumX],
-    [sumX, X.length]
-  ],
-  [sumXY, sumY],
+const linearApproximation = (
+  sumX2, sumX,
+  length,
+  sumXY, sumY,
+) => {
+  const [aCoefs, bCoefs] = lusolve(
+    [
+      [sumX2, sumX],
+      [sumX, length]
+    ],
+    [sumXY, sumY],
+  );
+  
+  const [aCoef] = aCoefs;
+  const [bCoef] = bCoefs;
+
+  return {
+    a: aCoef,
+    b: bCoef,
+  };
+};
+
+const quadraticApproximation = (
+  sumX4, sumX3, sumX2,
+  sumX2Y, sumXY, sumY,
+  sumX, length,
+) => {
+
+  const [aCoefs, bCoefs, cCoefs] = lusolve(
+    [
+      [sumX4, sumX3, sumX2],
+      [sumX3, sumX2, sumX],
+      [sumX2, sumX, length],
+    ],
+    [sumX2Y, sumXY, sumY],
+  );
+
+  console.log('aCoefs', aCoefs);
+  console.log('bCoefs', bCoefs);
+  console.log('cCoefs', cCoefs);
+  
+  const [aCoef] = aCoefs;
+  const [bCoef] = bCoefs;
+  const [cCoef] = cCoefs;
+
+  return {
+    a: aCoef,
+    b: bCoef,
+    c: cCoef,
+  };
+};
+
+const { a, b, c } = quadraticApproximation(
+  sumX4, sumX3, sumX2,
+  sumX2Y, sumXY, sumY,
+  sumX, X.length,
 );
 
-const [aCoef] = aCoefs;
-const [bCoef] = bCoefs;
-
-console.log('aCoef', aCoef);
-console.log('bCoef', bCoef);
+console.log('aCoef', a);
+console.log('bCoef', b);
+console.log('cCoef', c);
 
 console.log('\n');
-console.log(`f(x) = ${aCoef}x + ${bCoef}`);
+// console.log(`f(x) = ${aCoef}x + ${bCoef}`);
+console.log(`f(x) = ${a}x^2 + ${b}x + ${c}`);
--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,93 @@ const fpFix = (n) => (
   Math.round(n * 100) / 100
 );
 
-const calculateApproximation = (X, Y) => {
+const drawChart = (X, Y, approximateLinearY, approximateQuadraticY) => {
+  const ctx = document.getElementById('chart').getContext('2d');
+  const chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      datasets: [
+        {
+          backgroundColor: 'transparent',
+          pointBackgroundColor: 'red',
+          pointBorderColor: 'red',
+          borderColor: 'red',
+          label: 'y = kx + b',
+          data: X.map((x, i) => ({
+            x,
+            y: approximateLinearY[i],
+          })),
+        },
+        {
+          backgroundColor: 'transparent',
+          pointBackgroundColor: 'green',
+          pointBorderColor: 'green',
+          borderColor: 'green',
+          // lineTension: 0.3,
+          cubicInterpolationMode: 'monotone',
+          label: 'y = ax^2 + bx + c',
+          data: X.map((x, i) => ({
+            x,
+            y: approximateQuadraticY[i],
+          })),
+        },
+        {
+          backgroundColor: 'transparent',
+          pointBackgroundColor: 'blue',
+          pointBorderColor: 'blue',
+          borderColor: 'transparent',
+          label: 'dots',
+          data: X.map((x, i) => ({
+            x,
+            y: Y[i],
+          })),
+        },
+      ],
+    },
+    options: {
+      scales: {
+        xAxes: [{
+          type: 'linear',
+          position: 'bottom',
+          ticks: {
+            maxRotation: 0
+          },
+          scaleLabel: {
+            labelString: 'X',
+            display: true,
+          },
+        }],
+        yAxes: [{
+          type: 'linear',
+          ticks: {
+            maxRotation: 0
+          },
+          scaleLabel: {
+            labelString: 'Y',
+            display: true
+          }
+        }]
+      },
+      responsive:true,
+      maintainAspectRatio: false,
+
+      plugins: {
+        zoom: {
+          pan: {
+            enabled: true,
+            mode: 'xy'
+          },
+          zoom: {
+            enabled: true,
+            mode: 'xy'
+          }
+        }
+      },
+    }
+  });
+};
+
+const linearApproximation = (X, Y) => {
   const sumX = X.reduce((acc, current) => acc + current, 0);
   const sumY = Y.reduce((acc, current) => acc + current, 0);
   const sumXSquared = X.reduce((acc, current) => acc + current ** 2, 0);
@@ -18,52 +104,42 @@ const calculateApproximation = (X, Y) => {
   
   const [aCoef] = aCoefs;
   const [bCoef] = bCoefs;
- 
-  const aCoefFixed = fpFix(aCoef);
-  const bCoefFixed = fpFix(bCoef);
 
-  console.log('\n');
-  console.log(`f(x) = ${aCoefFixed}x + ${bCoefFixed}`);
-
-  const approximateY = X.map(x => fpFix(aCoefFixed * x + bCoefFixed));
-
-  const ctx = document.getElementById('chart').getContext('2d');
-  const chart = new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: X,
-      datasets:[
-        {
-          type: 'line',
-          label: '# 2 of Votes',
-          lineTension: 0,
-          data: approximateY,
-          backgroundColor: "transparent",
-          borderColor: "rgba(218,83,79, 1)",
-          borderWidth: 3,
-        },
-        {
-          type: 'bubble',
-          label: '# 1 of Votes',
-          data: Y,
-          backgroundColor: 'rgba(25, 25, 200, 1)',
-          borderColor: 'rgba(25, 25, 200, 1)',
-          borderWidth: 2,
-        },
-      ]
-    },
-    options: {
-      elements: {
-        line: {
-            tension: 0
-        },
-      },
-    },
-  });
+  return {
+    a: aCoef,
+    b: bCoef,
+  };
 };
 
-const X = [1, 2, 3, 4, 5];
-const Y = [5.3, 6.3, 4.8, 3.8, 3.3];
+const quadraticApproximation = (X, Y) => {
+  const sumX = X.reduce((acc, current) => acc + current);
+  const sumX2 = X.reduce((acc, current) => acc + current ** 2, 0);
+  const sumX3 = X.reduce((acc, current) => acc + current ** 3, 0);
+  const sumX4 = X.reduce((acc, current) => acc + current ** 4, 0);
+
+  const sumY = Y.reduce((acc, current) => acc + current);
+  const sumXY = X.reduce((acc, current, i) => acc + current * Y[i], 0);
+  const sumX2Y = X.reduce((acc, current, i) => acc + current ** 2 * Y[i] , 0);
+
+  const [aCoefs, bCoefs, cCoefs] = math.lusolve(
+    [
+      [sumX4, sumX3, sumX2],
+      [sumX3, sumX2, sumX],
+      [sumX2, sumX, X.length],
+    ],
+    [sumX2Y, sumXY, sumY],
+  );
+
+  const [aCoef] = aCoefs;
+  const [bCoef] = bCoefs;
+  const [cCoef] = cCoefs;
+
+  return {
+    a: aCoef,
+    b: bCoef,
+    c: cCoef,
+  };
+};
 
 const addDot = (container) => {
   const inputs = document.createElement('div');
@@ -97,18 +173,30 @@ const getDotsValues = () => {
   return { x, y };
 };
 
+const setCoef = (node, value) => node.innerHTML = value;
+
 window.onload = () => {
   const calculateButton = document.getElementById('calculate');
   const addDotButton = document.getElementById('add-dot');
   const inputsContainer = document.getElementById('inputs-container');
 
+
   calculateButton.addEventListener('click', () => {
     const { x, y } = getDotsValues(inputsContainer);
+    const linearCoefs = linearApproximation(x, y);
+    const quadraticCoefs = quadraticApproximation(x, y);
 
-    console.log('x', x);
-    console.log('y', y);
+    const linearY = x.map(x => linearCoefs.a * x + linearCoefs.b);
+    const quadraticY = x.map(x => quadraticCoefs.a * x * x + quadraticCoefs.b * x + quadraticCoefs.c);
 
-    calculateApproximation(x, y);
+    drawChart(x, y, linearY, quadraticY);
+    
+    setCoef(document.getElementById('a_1'), `a: ${linearCoefs.a}`);
+    setCoef(document.getElementById('b_1'), `b: ${linearCoefs.b}`);
+
+    setCoef(document.getElementById('a_2'), `a: ${quadraticCoefs.a}`);
+    setCoef(document.getElementById('b_2'), `b: ${quadraticCoefs.b}`);
+    setCoef(document.getElementById('c_2'), `c: ${quadraticCoefs.c}`);
   });
 
   addDotButton.addEventListener('click', () => {
@@ -116,6 +204,6 @@ window.onload = () => {
   });
 
   // for intial chart render
-  calculateApproximation([], []);
+  drawChart([], [], []);
 }
 
