2a0acd3766536f823c675bcfa9b456d0c8ad3c91
Vitalii Shapoval <vitalii.shapoval@starnavi.io>
2019-11-27 09:00:40 +0200

    finish 3 lab

---
+++ gauss.js
@@ -0,0 +1,17 @@
+const {
+  lusolve,
+} = require('mathjs');
+
+const 
+
+
+const result = lusolve(
+  [
+    [4, 2, 1],
+    [5, 1, 5],
+    [1, 4, 2],
+  ],
+  [4, 1, 5],
+);
+
+console.log('result', result);
--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,90 @@
-const drawChart = (dots, linear, lagrange) => {
+function chartClicked(event, chart) {
+  const yTop = chart.chartArea.top;
+  const yBottom = chart.chartArea.bottom;
+
+  const yMin = chart.scales['y-axis-0'].min;
+  const yMax = chart.scales['y-axis-0'].max;
+  let newY = 0;
+
+  if (event.offsetY <= yBottom && event.offsetY >= yTop) {
+    newY = Math.abs((event.offsetY - yTop) / (yBottom - yTop));
+    newY = (newY - 1) * -1;
+    newY = newY * (Math.abs(yMax - yMin)) + yMin;
+  }
+
+  const xTop = chart.chartArea.left;
+  const xBottom = chart.chartArea.right;
+  const xMin = chart.scales['x-axis-0'].min;
+  const xMax = chart.scales['x-axis-0'].max;
+  let newX = 0;
+
+  if (event.offsetX <= xBottom && event.offsetX >= xTop) {
+    newX = Math.abs((event.offsetX - xTop) / (xBottom - xTop));
+    newX = newX * (Math.abs(xMax - xMin)) + xMin;
+  }
+
+  console.log(newX, newY);
+
+  return {
+    x: newX,
+    y: newY,
+  };
+}
+
+const addDot = (container) => {
+  const inputs = document.createElement('div');
+  const inputX = document.createElement('input');
+  const inputY = document.createElement('input');
+
+  inputs.classList.add('inputs');
+  inputX.setAttribute('placeholder', 'x');
+  inputY.setAttribute('placeholder', 'y');
+
+  inputs.appendChild(inputX);
+  inputs.appendChild(inputY);
+
+  container.appendChild(inputs);
+};
+
+const getDotsValues = () => {
+  const inputs = document.querySelectorAll('.inputs');
+  const inputsData = [];
+
+  inputs.forEach((input) => {
+    const x = input.children[0].value;
+    const y = input.children[1].value;
+
+    inputsData.push({ x, y });
+  });
+
+  const x = inputsData.map((item) => +item.x);
+  const y = inputsData.map((item) => +item.y);
+
+  return { x, y };
+};
+
+window.onload = () => {
+  const calculateButton = document.getElementById('calculate');
+  const addDotButton = document.getElementById('add-dot');
+  const inputsContainer = document.getElementById('inputs-container');
+  // const step = document.getElementById('step');
+  // const result = document.getElementById('resultDot');
+  const editBtn = document.getElementById('edit');
+
+  let isEditable = true;
+
+  editBtn.addEventListener('click', () => {
+    if (isEditable) {
+      editBtn.innerHTML = 'Enable edit';
+      isEditable = false;
+    } else {
+      editBtn.innerHTML = 'Disable edit';
+      isEditable = true;
+    }
+  });
+
+
+  const canvas = document.getElementById('chart');
   const ctx = document.getElementById('chart').getContext('2d');
   // eslint-disable-next-line
   const chart = new Chart(ctx, {
@@ -9,9 +95,11 @@ const drawChart = (dots, linear, lagrange) => {
           backgroundColor: 'transparent',
           pointBackgroundColor: 'blue',
           pointBorderColor: 'blue',
-          borderColor: 'transparent',
+          borderColor: 'blue',
+          lineTension: 0,
+          cubicInterpolationMode: 'monotone',
           label: 'dots',
-          data: dots,
+          data: [],
         },
         {
           backgroundColor: 'transparent',
@@ -20,16 +108,28 @@ const drawChart = (dots, linear, lagrange) => {
           borderColor: 'green',
           // lineTension: 0.3,
           cubicInterpolationMode: 'monotone',
-          label: 'linear',
-          data: linear,
+          label: 'gaus_1',
+          data: [],
         },
         {
           backgroundColor: 'transparent',
           pointBackgroundColor: 'red',
           pointBorderColor: 'red',
           borderColor: 'red',
-          label: 'Lagrange',
-          data: lagrange,
+          // lineTension: 0.3,
+          cubicInterpolationMode: 'monotone',
+          label: 'gaus_2',
+          data: [],
+        },
+        {
+          backgroundColor: 'transparent',
+          pointBackgroundColor: 'yellow',
+          pointBorderColor: 'yellow',
+          borderColor: 'yellow',
+          // lineTension: 0.3,
+          cubicInterpolationMode: 'monotone',
+          label: 'gaus_3',
+          data: [],
         },
       ],
     },
@@ -74,147 +174,82 @@ const drawChart = (dots, linear, lagrange) => {
       },
     },
   });
-};
 
-const findRange = (arr, value) => {
-  for (let i = 0; i < arr.length - 1; i += 1) {
-    if (value >= arr[i] && value <= arr[i + 1]) {
-      return i;
-    }
-  }
-};
-
-const calculateLagrange = (x, X, Y) => {
-  let lagrangePol = 0;
-
-  // TODO: do smth with fors
-  for (let i = 0; i < X.length; i += 1) {
-    let basicsPol = 1;
-
-    for (let j = 0; j < X.length; j += 1) {
-      if (i === j) continue;
-
-      basicsPol *= (x - X[j]) / (X[i] - X[j]);
+  const dotsArr = [];
+  canvas.addEventListener('click', (e) => {
+    if (!isEditable) {
+      return;
     }
 
-    lagrangePol += basicsPol * Y[i];
-  }
+    const newDot = chartClicked(e, chart);
 
-  return lagrangePol;
-};
-
-// TODO: rename
-const calculateLinear = (X, Y) => {
-  const a = [];
-  const b = [];
-
-  for (let i = 0; i < X.length - 1; i += 1) {
-    const ai = (Y[i + 1] - Y[i]) / (X[i + 1] - X[i]);
-    const bi = Y[i] - ai * X[i];
-
-    a.push(ai);
-    b.push(bi);
-  }
-
-  return {
-    a,
-    b,
-  };
-};
-
-const addDot = (container) => {
-  const inputs = document.createElement('div');
-  const inputX = document.createElement('input');
-  const inputY = document.createElement('input');
-
-  inputs.classList.add('inputs');
-  inputX.setAttribute('placeholder', 'x');
-  inputY.setAttribute('placeholder', 'y');
-
-  inputs.appendChild(inputX);
-  inputs.appendChild(inputY);
-
-  container.appendChild(inputs);
-};
-
-const getDotsValues = () => {
-  const inputs = document.querySelectorAll('.inputs');
-  const inputsData = [];
-
-  inputs.forEach((input) => {
-    const x = input.children[0].value;
-    const y = input.children[1].value;
-
-    inputsData.push({ x, y });
+    dotsArr.push(newDot);
+    chart.data.datasets[0].data = [...dotsArr];
+    chart.update();
   });
 
-  const x = inputsData.map((item) => +item.x);
-  const y = inputsData.map((item) => +item.y);
-
-  return { x, y };
-};
-
-window.onload = () => {
-  const calculateButton = document.getElementById('calculate');
-  const addDotButton = document.getElementById('add-dot');
-  const inputsContainer = document.getElementById('inputs-container');
-  const step = document.getElementById('step');
-  const result = document.getElementById('resultDot');
-  const resultBtn = document.getElementById('result');
-
-  calculateButton.addEventListener('click', () => {
-    const {
-      x,
-      y,
-    } = getDotsValues();
-
-    // Linear
-    const {
-      a,
-      b,
-    } = calculateLinear(x, y);
-
-    const newX = [];
-    const newF = [];
-
-    // TODO: use reduce
-    for (let i = x[0]; i <= x[x.length - 1]; i += +step.value) {
-      newX.push(i);
+  calculateButton.addEventListener('click', async () => {
+    if (!isEditable) {
+      return;
     }
-    // TODO: refactor: use reduce
-    newX.forEach((item) => {
-      const index = findRange(x, item);
-      newF.push(a[index] * item + b[index]);
+
+    // TODO: do math
+    const data = await fetch('http://localhost:5000/api/v1/gauss_1', {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({
+        x: dotsArr.map((item) => item.x),
+        y: dotsArr.map((item) => item.y),
+        step: 0.1,
+      }),
     });
-
-    // Lagrange
-    const lagrangeF = newX.map((xi) => calculateLagrange(xi, x, y));
-
-    drawChart(
-      x.map((x, i) => ({ x, y: y[i] })),
-      newX.map((x, i) => ({ x, y: newF[i] })),
-      newX.map((x, i) => ({ x, y: lagrangeF[i] })),
-    );
-
-    resultBtn.addEventListener('click', () => {
-      const index = newX.findIndex(x => x === +result.value);
-      // console.log('index', index);
-
-      // console.log('newF', newF[index]);
-      // console.log('larg', lagrangeF[index]);
-
-      alert(`\n linear: ${newF[index]} \n Largange: ${lagrangeF[index]} \n`);
+    const parsedData = await data.json();
+    const data2 = await fetch('http://localhost:5000/api/v1/gauss_2', {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({
+        x: dotsArr.map((item) => item.x),
+        y: dotsArr.map((item) => item.y),
+        step: 0.1,
+      }),
     });
-  });
+    const parsedData2 = await data2.json();
 
-  addDotButton.addEventListener('click', () => {
-    addDot(inputsContainer);
-  });
+    const data3 = await fetch('http://localhost:5000/api/v1/gauss_3', {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({
+        x: dotsArr.map((item) => item.x),
+        y: dotsArr.map((item) => item.y),
+        step: 0.1,
+      }),
+    });
+    const parsedData3 = await data3.json();
 
-  // * Needs for initial chart render
-  drawChart(
-    [],
-    [],
-    [],
-  );
+    chart.data.datasets[1].data = parsedData.new_x.map((item, i) => ({
+      x: item,
+      y: parsedData.new_y[i],
+    }));
+    chart.data.datasets[2].data = parsedData2.new_x.map((item, i) => ({
+      x: item,
+      y: parsedData2.new_y[i],
+    }));
+    chart.data.datasets[3].data = parsedData3.new_x.map((item, i) => ({
+      x: item,
+      y: parsedData3.new_y[i],
+    }));
+    chart.update();
+  });
 };
